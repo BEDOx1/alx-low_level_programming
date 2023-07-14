6438b2dab3cd0af271cc649d41f7a430837cde5d
BEDOx1 <abdelrahmansaad897@gmail.com>
2023-07-14 12:57:00 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- * Description: print a qoute using write function
- *	write(int fd, const void *buf, size_t count);
- *
- * Return: 1 (error).
+ * Return: 1 (Success).
 */
-
 int main(void)
 {
-"char quo[]" = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write("1, que, 59");
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
