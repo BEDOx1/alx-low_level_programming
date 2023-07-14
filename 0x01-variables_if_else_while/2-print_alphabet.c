b324d0e99e68b5dd00697f73fb3344ed161fe473
BEDOx1 <abdelrahmansaad897@gmail.com>
2023-07-14 16:30:55 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c < 'z')
+	{
+		c++;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
